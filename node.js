7499e59d1129a26263ac6180bcb9beb49ec9f27c
KAUAN ALYSSON SILVA <kauan_alysson-silva@estudante.sesisenai.org.br>
2025-05-22 08:46:31 -0300

    parte1

---
+++ node.js
@@ -0,0 +1,11 @@
+const express =  require('express');
+const fs = require('fs');
+const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+
+const PORTA = 8000;
+
+function registraLog(id , datahora, aluno) {
+    const
+}
