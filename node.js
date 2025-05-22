40a73394d51c5e64ddf197a7d84c8e6ae3fe2658
KAUAN ALYSSON SILVA <kauan_alysson-silva@estudante.sesisenai.org.br>
2025-05-22 09:04:45 -0300

    parte 3

--- node.js
+++ node.js
@@ -11,5 +11,14 @@ function registrarLog(nome) {
     const id = uuidv4();
     const dataHora = new Date().toISOString().replace('T', ' ').slice(0, 19);
     const mensagem = `${id} - ${dataHora} - ${nome}\n`;
-}
+   
+    fs.appendFileSync('logs.txt', mensagem);
+    return { id, mensagem };
+}  
   
+//adcionar log
+app.post('/logs', (req, res) => {
+    const { nome } = req.body;
+  
+}  
+
